77dd1f8bab23bca72c0b439b35d74bcb7ab094ff
Renan Reis <renanmacfernandes@hotmail.com>
2024-09-17 12:25:11 -0300

    criation of cadastration page

---
+++ src/app/pages/cadastration/cadastration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastration',
+  templateUrl: './cadastration.component.html',
+  styleUrl: './cadastration.component.css'
+})
+export class CadastrationComponent {
+
+}
