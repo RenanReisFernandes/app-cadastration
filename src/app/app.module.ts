6f24e99610e20fea2d08c6d3aba03c3dffa5f60d
Renan Reis <renanmacfernandes@hotmail.com>
2024-09-17 13:39:05 -0300

    criation of component navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastrationComponent } from './pages/cadastration/cadastration.component';
+import { NavBarComponent } from './_components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CadastrationComponent
+    CadastrationComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
