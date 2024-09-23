f15f029eb9ea958cdc41dc6ca01dec9dbfc81162
Renan Reis <renanmacfernandes@hotmail.com>
2024-09-23 17:04:25 -0300

    creating table Paciente-list

--- src/app/pages/paciente-list/paciente-list.component.ts
+++ src/app/pages/paciente-list/paciente-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Paciente } from '../../entities/paciente';
 
 @Component({
   selector: 'app-paciente-list',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class PacienteListComponent {
 
+  pacientes: Paciente[];
+
+  constructor(){}
+  ngOnInit(): void{
+
+  }
 }
