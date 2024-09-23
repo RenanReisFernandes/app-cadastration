e3d019d0b09f195108e55f2c3fd995bccbd3c848
Renan Reis <renanmacfernandes@hotmail.com>
2024-09-23 16:53:02 -0300

    creating routing's Paciente-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrationComponent } from './pages/cadastration/cadastration.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PacienteListComponent } from './pages/paciente-list/paciente-list.component';
 
 const routes: Routes = [
   {path: '', component: CadastrationComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'pacientes',component: PacienteListComponent}
 ];
 
 @NgModule({
